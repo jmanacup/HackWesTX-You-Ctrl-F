616e7b17e831f4189f0a1a72ef41f17b2fe58966
John Manacup <john.manacup@gmail.com>
2020-03-07 16:26:12 -0600

    url for video id

--- popup.js
+++ popup.js
@@ -4,8 +4,9 @@
             chrome.tabs.query({currentWindow: true, active: true},
                 function(tabs){
                     chrome.tabs.sendMessage(tabs[0].id,'hii')
-                    var url = tabs[0].url;
+                    var url = tabs[0].url.split("v=")[1].substring(0, 11);
                     chrome.tabs.sendMessage(tabs[0].id, url)
+
             });
         }   
   
