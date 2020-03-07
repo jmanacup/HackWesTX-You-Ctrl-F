f91c65af9febbc19f42f1b8b92659331992a4914
John Manacup <john.manacup@gmail.com>
2020-03-07 16:54:39 -0600

    word can be stored now

--- popup.js
+++ popup.js
@@ -3,9 +3,11 @@
         function onclick (){
             chrome.tabs.query({currentWindow: true, active: true},
                 function(tabs){
-                    chrome.tabs.sendMessage(tabs[0].id,'hii')
+                    chrome.tabs.sendMessage(tabs[0].id,'hii');
+
+                    //This is for the video id
                     var url = tabs[0].url.split("v=")[1].substring(0, 11);
-                    chrome.tabs.sendMessage(tabs[0].id, url)
+                    var word = document.getElementById('word').value;
 
             });
         }   
