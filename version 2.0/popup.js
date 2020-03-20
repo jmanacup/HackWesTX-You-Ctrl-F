a0bcb381000d68dfdbad487c8e9f1a0bc3c3d691
John Manacup <john.manacup@gmail.com>
2020-03-19 19:37:09 -0500

    version 2.0 working

--- version 2.0/popup.js
+++ version 2.0/popup.js
@@ -1,108 +1,116 @@
-counter = 1;
-    document.querySelector('button').addEventListener('click', onclick, false)
-        function onclick (e){
-            console.log(e);
-            chrome.tabs.query({currentWindow: true, active: true},
-                function(tabs){
+    
+var reset = false;
 
-                    //This is for the video id
-                    var url = tabs[0].url.split("v=")[1].substring(0, 11);
-                    var originalUrl = tabs[0].url;
-                    //this is for the word to be searched
-                    var word = document.getElementById('word').value;
+document.querySelector('button').addEventListener('click', onclick, false)
+    function onclick (e){
+        console.log(e);
+        chrome.tabs.query({currentWindow: true, active: true},
+            function(tabs){
 
-                    let entirePath = "https://www.youtube.com/api/timedtext?lang=en&v=" + String(url);
+                //This is for the video id
+                var url = tabs[0].url.split("v=")[1].substring(0, 11);
+                var originalUrl = tabs[0].url;
+                //this is for the word to be searched
+                var word = document.getElementById('word').value;
 
-                    //to download the xml file
-/*
-                    chrome.downloads.download({
-                        url: String(entirePath),
-                        filename: String(word) + counter + ".xml",
-                      });
+                let entirePath = "https://www.youtube.com/api/timedtext?lang=en&v=" + String(url);
 
-                    counter++;
-*/
-  //                  extractXMLData(word, originalUrl);
-     
+                //fetch the XML data
+                let timedText;
+                fetch(entirePath)
+                    .then(response => response.text())
+                    .then(result => {
+                        timedText = extractXMLData(result);
+                        showData(timedText, word, originalUrl);
+                        reset = true;
+                    })
+                    .catch(error => alert(error));
 
-                    fetch(entirePath)
-                        .then(response => response.text())
-                        .then(result => alert(result))
-                        .catch(error => alert(error));
-                
-                });
-    }
+            });
+}
   
-function extractXMLData(word, originalUrl){
+function extractXMLData(result){
 
-    const input = document.querySelector('input[type="file"]')
-    input.addEventListener('change',function(e){
-    //console.log(input.file) 
-    const reader = new FileReader()
-
-    var i=0,number=0,text,j=0,k=0, l=0;
+    var i=0,number=0,text,j=0,k=0;
     var timedText = new Array(2);
     timedText[0] = new Array(10000);
     timedText[1] = new Array(10000);
 
-    reader.onload=function(){
-    const line=reader.result
+    let line = result.toLowerCase();
 
     while(i<line.length){
     if (line.indexOf('<',i)!=-1)
     {
-        i=line.indexOf('<',i)
+        i=line.indexOf('<',i);
         
         if (line.indexOf('<text',i)==i)
         {
             i=line.indexOf('<text',i)
-            i+=13
-            j=i
+            i+=13;
+            j=i;
             while(line[j]!='.')
             {
-                j++
+                j++;
             }
             number=line.substring(i,j)
             timedText[0][k]=number
-            console.log("TimeStamp: "+timedText[0][k])
-            //console.log(timedText[0][k])
-            i=line.indexOf('>',i)
-            i+=1
-            j=i
+            i=line.indexOf('>',i);
+            i+=1;
+            j=i;
             while(line[j]!='<')
             {
-                j++
+                j++;
             }
-            text=line.substring(i,j)
-            text=text.replace("&amp;#39;",'\'')
+            text=line.substring(i,j);
+            text=text.replace("&amp;#39;",'\'');
                 
-            timedText[1][k]=text
-            console.log("TimedText: "+timedText[1][k])
-            //console.log(timedText[1][k])
-            k++
+            timedText[1][k]=text;
+            k++;
         }
         else
         {
-            i=line.indexOf('>',i)
+            i=line.indexOf('>',i);
         }
     }
-    i++
+    i++;
     }
 
     //show data based on the timedText
-    showData(timedText, word, originalUrl);
+    return timedText;
 
-    }
-
-    reader.readAsText(input.files[0])
-    }, false)
 }
 
 function showData(timedText, word, originalUrl){
 
+    if(reset){ //to clear the result if user search for another one
+        var elements = document.getElementsByTagName("div");
+        var elements2 = document.getElementsByTagName('a');
+        var elements3 = document.getElementsByClassName("headerTitle");
+        for(var i = 0; i < elements.length; i++){
+            elements[i].innerHTML = "";
+        }
+
+        for(var j = 0; j < elements2.length; j++){
+            elements2[j].innerHTML = "";
+        }
+        for(var k = 0; k < elements3.length; k++){
+            elements3[k].innerHTML = "";
+        }
+
+    }
+
+
     var isThere = false;
+    var l;
+    
+    var header = document.createElement('h2');
+    header.innerHTML = "HERE ARE THE RESULTS FOR THE WORD "  + word.toUpperCase();
+    header.className = "headerTitle";
+    document.getElementsByTagName('body')[0].appendChild(header);
+
     for(l = 0; l < timedText[0].length; l++){
     if(typeof timedText[1][l] !== "undefined"){
+        word = word.toLowerCase();
         var str_pos = timedText[1][l].search(word);
         if(str_pos > -1){
 
@@ -112,21 +120,20 @@ function showData(timedText, word, originalUrl){
         var min = temp / 60;
         var sec = temp % 60;
 
-        min = min.toPrecision(1);
-        
+        min = min.toFixed();
 
         var alink = document.createElement('a');
-        var link = document.createTextNode(min + ':' + sec);
-        alink.appendChild(link);
+        alink.innerHTML = min + ':' + (sec < 10 ? '0' : '') + sec; 
         alink.title = min + ':' + sec;
         alink.href = originalUrl + "&t=" + timedText[0][l];
-        document.body.appendChild(alink);
+        alink.id = "timeLink";
+        document.getElementsByTagName('body')[0].appendChild(alink);
             
-            isThere = true;
+        isThere = true;
 
-            //text
-            var text = timedText[1][l]; //<<< The text you want to display
-            var createText = document.createElement('div'); //<<< create a Element
+        //text
+        var text = timedText[1][l]; //<<< The text you want to display
+        var createText = document.createElement('div'); //<<< create a Element
         createText.innerHTML = text; // <<< append content to the element
         createText.id = "resultWords";
         document.getElementsByTagName('body')[0].appendChild(createText);
@@ -134,7 +141,7 @@ function showData(timedText, word, originalUrl){
     }
     }
     if(!isThere)
-    console.log("There is no caption for the word " + word);
+        alert("There is no caption for the word " + word);
 
 }
 
